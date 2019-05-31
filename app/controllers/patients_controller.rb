100f8ed8b2e9bd660de0ad53d9f3273a049fb9a7
Evelyn Rosas <Evelynvrosas@gmail.com>
2019-05-30 19:17:46 -0600

    added patient controller

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,38 @@
+class PatientsController < ApplicationController
+  before_action :set_patient, only: [:show, :destroy]
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+
+  if 
+  @patient.save
+  redirect_to @patient
+
+  else
+    render :new
+  end
+  end
+
+  def destroy
+    @patient.destroy
+    redirect_to patient_path
+  end
+  
+  private
+    def set_patient
+      @paitnet =Patient.find(params[:id])
+
+  def patient_params
+    params.require(:patient).permit(:patient_name)
+    end
