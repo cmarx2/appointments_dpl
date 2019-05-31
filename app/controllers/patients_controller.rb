116b5599571b77b07c9128390f4d19603b2e3ad7
Evelyn Rosas <Evelynvrosas@gmail.com>
2019-05-30 19:32:31 -0600

    cleaned up controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,11 +1,12 @@
 class PatientsController < ApplicationController
   before_action :set_patient, only: [:show, :destroy]
+  
   def index
     @patients = Patient.all
   end
 
   def show
-
+    # Before Action
   end
 
   def new
@@ -14,25 +15,27 @@ class PatientsController < ApplicationController
 
   def create
     @patient = Patient.new(patient_params)
-
-  if 
-  @patient.save
-  redirect_to @patient
-
-  else
+    if 
+      @patient.save
+      redirect_to @patient
+    else
     render :new
-  end
+    end
   end
 
   def destroy
     @patient.destroy
     redirect_to patient_path
   end
-  
+
   private
+
     def set_patient
       @paitnet =Patient.find(params[:id])
+    end
 
-  def patient_params
+    def patient_params
     params.require(:patient).permit(:patient_name)
     end
+
+end
