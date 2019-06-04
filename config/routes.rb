c731a9664f6d36a501fd6ace3ec636e5ef52daa2
Candice Marx <candice.marx@hotmail.com>
2019-06-03 20:47:41 -0600

    fixed error code

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
  root 'pages#home'
   get "/pages/:page" => "pages#show"
-  resources :physicians
-
-  resources :patients
+  resources :physicians do
+    resources :appointments
+  end
+  resources :patients do 
+  resources :appointments, only: [:index, :new, :create, :destroy ]
+  end
 end
